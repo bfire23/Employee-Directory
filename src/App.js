e8e2fb483b64b2f2183e52030f6bb5e18eb77a3a
Blake Arambula <bfire23@gmail.com>
2020-10-17 14:24:59 -0700

    created center.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Header from "./components/Header";
 import DataArea from './components/DataArea';
-import Search from "./components/Search";
 import Wrapper from "./components/Wrapper";
 import "./App.css";
+import Center from "./components/Center";
 
 
 
@@ -12,6 +12,7 @@ function App() {
     <div className= "App">
       <Wrapper>
         <Header />
+        <Center />
         <DataArea />
       </Wrapper>
     </div>
---
+++ src/components/Center.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "./DataArea";
+
+
+function Center (){
+    return (
+        <>
+        <DataArea />
+        </>
+    );
+};
+
+export default Center;
